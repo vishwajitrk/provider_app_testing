05c1186952a393b92fee760ec77b3f2691e2bd38
Vishwajit Ramesh Khairnar <Vishwajit.Khairnar@Triconinfotech.com>
2022-03-06 23:38:20 +0530

    pass widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@ void main() {
     test('A new item should be added', () {
       var number = 35;
       favorites.add(number);
-      expect(favorites.items.contains(46), true);
+      expect(favorites.items.contains(number), true);
     });
   });
 }
