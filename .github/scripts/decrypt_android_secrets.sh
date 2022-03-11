bd8c9756f3c7d1cfd67c1dc4bc5afda3119d760a
vishwajit-khairnar1 <Vishwajit.Khairnar@mheducation.com>
2022-03-11 15:26:58 +0530

    update fastlane config

--- .github/scripts/decrypt_android_secrets.sh
+++ .github/scripts/decrypt_android_secrets.sh
@@ -2,5 +2,17 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="Testing123" \
+gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_SECRET_PASSPHRASE" \
 --output android/android_keys.zip android/android_keys.zip.gpg && cd android && jar xvf android_keys.zip && cd -
+echo $PWD
+
+echo "********1"
+ls -d $PWD/android/*
+mv ./android/expensemanager.jks ./android/app
+mv ./android/google-services.json ./android/app
+echo ./
+echo "********2"
+FILE=./android/app/upload-key.jks
+if test -f "$FILE"; then
+    echo "$FILE exists."
+fi
